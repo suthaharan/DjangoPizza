57c59fa3889b8f101e0c0ab868d586cd9e0b81e1
sutha <sutha@kurinchilion.com>
2024-05-13 12:22:32 -0400

    multiple forms

--- pizza/forms.py
+++ pizza/forms.py
@@ -9,7 +9,7 @@ from .models import Pizza, Size
 
 class PizzaForm(forms.ModelForm):
     size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
-    image = forms.ImageField()
+    # image = forms.ImageField()
     class Meta:
             model = Pizza
             fields = ['topping1', 'topping2', 'size']
